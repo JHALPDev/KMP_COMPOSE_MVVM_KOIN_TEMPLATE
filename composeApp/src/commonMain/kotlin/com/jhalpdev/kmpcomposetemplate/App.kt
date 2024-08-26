8e1df9095fcc637b21df29d2ec16af11a4e89602
JosePalomino <jose.alcarrazpalo@gmail.com>
2024-08-26 15:16:13 +0200

    minor fix

--- composeApp/src/commonMain/kotlin/com/jhalpdev/kmpcomposetemplate/App.kt
+++ composeApp/src/commonMain/kotlin/com/jhalpdev/kmpcomposetemplate/App.kt
@@ -2,9 +2,6 @@ package com.jhalpdev.kmpcomposetemplate
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import com.jhalpdev.kmpcomposetemplate.ui.MainScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -12,7 +9,6 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        val showContent by remember { mutableStateOf(false) }
-        MainScreen(showContent)
+        MainScreen()
     }
 }
--- composeApp/src/commonMain/kotlin/com/jhalpdev/kmpcomposetemplate/ui/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/jhalpdev/kmpcomposetemplate/ui/MainScreen.kt
@@ -7,7 +7,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.jhalpdev.kmpcomposetemplate.repositories.Greeting
@@ -16,13 +19,14 @@ import kmpcomposetemplate.composeapp.generated.resources.compose_multiplatform
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
-fun MainScreen(showContent: Boolean) {
-    var showContent1 = showContent
+fun MainScreen() {
+    var showContent by remember { mutableStateOf(false) }
+
     Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-        Button(onClick = { showContent1 = !showContent1 }) {
+        Button(onClick = { showContent = !showContent }) {
             Text("Click me!")
         }
-        AnimatedVisibility(showContent1) {
+        AnimatedVisibility(showContent) {
             val greeting = remember { Greeting().greet() }
             Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
                 Image(painterResource(Res.drawable.compose_multiplatform), null)
